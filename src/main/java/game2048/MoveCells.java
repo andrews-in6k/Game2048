a76318e5724f89cc952eb518767a98be350197de
Andriy Slipenko <andrews@interlink-ua.com>
2015-10-18 00:13:02 +0300

    moveUp paste test

--- src/main/java/game2048/MoveCells.java
+++ src/main/java/game2048/MoveCells.java
@@ -6,6 +6,32 @@ package game2048;
 public class MoveCells {
 
     public void moveUp(Cell[][] cell) {
+        for (int j = 0; j < GameField.FIELD_SIZE; j++) {
+            for (int i = 0; i < GameField.FIELD_SIZE - 1; i++) {
+                if (cell[i][j].getCellValue() != 0) {
+                    int k = i + 1;
+                    while (k < GameField.FIELD_SIZE) {
+                        if (cell[i][j].getCellValue() == cell[k][j].getCellValue()) {
+                            cell[i][j].incrementPower();
+                            cell[k][j].setCellValue(0);
+                            break;
+                        } else if (cell[k][j].getCellValue() != 0) {
+                            break;
+                        }
+                        k++;
+                    }
+                }
+            }
 
+            for (int i = 1; i < GameField.FIELD_SIZE; i++) {
+                if (cell[i][j].getCellValue() != 0) {
+                    while ((i != 0) && (cell[i - 1][j].getCellValue() == 0)) {
+                        cell[i - 1][j].setCellValue(cell[i][j].getCellValue());
+                        cell[i][j].setCellValue(0);
+                        i--;
+                    }
+                }
+            }
+        }
     }
 }
--- src/test/java/MoveCellsTest.java
+++ src/test/java/MoveCellsTest.java
@@ -22,14 +22,79 @@ public class MoveCellsTest {
     public void testMoveUp(){
         MoveCells moveCells = new MoveCells();
 
+        //test empty
         generateEmptyCells();
         moveCells.moveUp(cell);
+        assertThat(cellToString(), is(
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n"
+        ));
+
+        //case 1
+        generateMoveUpCase1();
+        moveCells.moveUp(cell);
+        assertThat(cellToString(), is(
+                "2 2 2 2 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n"
+        ));
+
+        //case 2
+        generateMoveUpCase2();
+        moveCells.moveUp(cell);
+        assertThat(cellToString(), is(
+                "2 0 2 2 \n" +
+                "4 0 4 4 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n"
+        ));
+
+        //case 3
+        generateMoveUpCase3();
+        moveCells.moveUp(cell);
+        assertThat(cellToString(), is(
+                "2 4 0 0 \n" +
+                "4 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n"
+        ));
+
+        //case 4
+        generateMoveUpCase4();
+        moveCells.moveUp(cell);
+        assertThat(cellToString(), is(
+                "4 8 16 4 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n"
+        ));
+
+        //case 5
+        generateMoveUpCase5();
+        moveCells.moveUp(cell);
+        assertThat(cellToString(), is(
+                "4 4 2 4 \n" +
+                "4 8 4 4 \n" +
+                "0 0 2 0 \n" +
+                "0 0 4 0 \n"
+        ));
+    }
+
+    private String cellToString() {
+        String cells = "";
+
         for (int i = 0; i < GameField.FIELD_SIZE; i++) {
             for (int j = 0; j < GameField.FIELD_SIZE; j++) {
-                assertThat(cell[i][j].getCellValue(), is(0));
+                cells += cell[i][j].getCellValue() + " ";
             }
+
+            cells += "\n";
         }
 
+        return cells;
     }
 
     private void generateEmptyCells() {
@@ -38,5 +103,67 @@ public class MoveCellsTest {
                 cell[i][j] = new Cell();
             }
         }
+    }
+
+    private void generateMoveUpCase1(){
+        generateEmptyCells();
+
+        cell[2][0] = new Cell(2);
+        cell[3][1] = new Cell(2);
+        cell[3][2] = new Cell(2);
+        cell[3][3] = new Cell(2);
+    }
+
+    private void generateMoveUpCase2(){
+        generateEmptyCells();
+
+        cell[2][0] = new Cell(2);
+        cell[3][0] = new Cell(4);
+        cell[0][2] = new Cell(2);
+        cell[3][2] = new Cell(4);
+        cell[0][3] = new Cell(2);
+        cell[1][3] = new Cell(4);
+    }
+
+    private void generateMoveUpCase3(){
+        generateEmptyCells();
+
+        cell[0][0] = new Cell(2);
+        cell[0][1] = new Cell(4);
+        cell[1][0] = new Cell(4);
+    }
+
+    private void generateMoveUpCase4(){
+        generateEmptyCells();
+
+        cell[0][0] = new Cell(2);
+        cell[0][1] = new Cell(4);
+        cell[1][0] = new Cell(2);
+        cell[1][2] = new Cell(8);
+        cell[1][3] = new Cell(2);
+        cell[2][1] = new Cell(4);
+        cell[2][3] = new Cell(2);
+        cell[3][2] = new Cell(8);
+    }
+
+    private void generateMoveUpCase5(){
+        generateEmptyCells();
+
+        cell[0][0] = new Cell(2);
+        cell[0][1] = new Cell(2);
+        cell[0][2] = new Cell(2);
+        cell[0][3] = new Cell(2);
+        cell[1][0] = new Cell(2);
+        cell[1][1] = new Cell(2);
+        cell[1][2] = new Cell(4);
+        cell[1][3] = new Cell(2);
+        cell[2][0] = new Cell(2);
+        cell[2][1] = new Cell(4);
+        cell[2][2] = new Cell(2);
+        cell[2][3] = new Cell(4);
+        cell[3][0] = new Cell(2);
+        cell[3][1] = new Cell(4);
+        cell[3][2] = new Cell(4);
+        cell[3][3] = new Cell(0);
     }
 }

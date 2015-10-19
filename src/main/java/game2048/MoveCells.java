da0dfc91410b3a37a8f54cea1f7168270f13fd5c
Andriy Slipenko <andrews@interlink-ua.com>
2015-10-19 12:31:09 +0300

    add ANSIGameFieldPrinterTest

--- src/main/java/game2048/MoveCells.java
+++ src/main/java/game2048/MoveCells.java
@@ -9,6 +9,10 @@ public class MoveCells {
 
     private Cell[][] cells;
 
+    private int row;
+    private int col;
+    private int tempIndex;
+
     public MoveCells(Cell[][] cells) {
         this.cells = cells;
 
@@ -16,185 +20,181 @@ public class MoveCells {
     }
 
     public boolean moveUp() {
-        for (int j = 0; j < GameField.FIELD_SIZE; j++) {
-            upCellsAddition(j);
-
-            upCellsShift(j);
+        for (col = 0; col < GameField.FIELD_SIZE; col++) {
+            upCellsAddition();
+            upCellsShift();
         }
 
         return hasMove;
     }
 
     public boolean moveDown() {
-        for (int j = 0; j < GameField.FIELD_SIZE; j++) {
-            downCellsAddition(j);
-
-            downCellsShift(j);
+        for (col = 0; col < GameField.FIELD_SIZE; col++) {
+            downCellsAddition();
+            downCellsShift();
         }
 
         return hasMove;
     }
 
     public boolean moveLeft(){
-        for (int i = 0; i < GameField.FIELD_SIZE; i++) {
-            leftCellsAddition(i);
-
-            leftCellsShift(i);
+        for (row = 0; row < GameField.FIELD_SIZE; row++) {
+            leftCellsAddition();
+            leftCellsShift();
         }
 
         return hasMove;
     }
 
     public boolean moveRight(){
-        for (int i = 0; i < GameField.FIELD_SIZE; i++) {
-            rightCellsAddition(i);
-
-            rightCellsShift(i);
+        for (row = 0; row < GameField.FIELD_SIZE; row++) {
+            rightCellsAddition();
+            rightCellsShift();
         }
 
         return hasMove;
     }
 
-    private void upCellsAddition(int j) {
-        for (int i = 0; i < GameField.FIELD_SIZE - 1; i++) {
-            if (cells[i][j].getCellValue() != 0) {
-                int k = i + 1;
+    private void upCellsAddition() {
+        for (row = 0; row < GameField.FIELD_SIZE - 1; row++) {
+            if (cells[row][col].getCellValue() != 0) {
+                tempIndex = row + 1;
 
-                while (k < GameField.FIELD_SIZE) {
-                    if (cells[i][j].getCellValue() == cells[k][j].getCellValue()) {
-                        verticalCellsAddition(j, i, k);
+                while (tempIndex < GameField.FIELD_SIZE) {
+                    if (cells[row][col].getCellValue() == cells[tempIndex][col].getCellValue()) {
+                        verticalCellsAddition();
                         break;
-                    } else if (cells[k][j].getCellValue() != 0) {
-                        break;
-                    }
-                    k++;
-                }
-            }
-        }
-    }
-
-    private void upCellsShift(int j) {
-        for (int i = 1; i < GameField.FIELD_SIZE; i++) {
-            if (cells[i][j].getCellValue() != 0) {
-                while ((i != 0) && (cells[i - 1][j].getCellValue() == 0)) {
-                    cells[i - 1][j].setCellValue(cells[i][j].getCellValue());
-                    cells[i][j].setCellValue(0);
-
-                    hasMove = true;
-                    i--;
-                }
-            }
-        }
-    }
-
-    private void downCellsAddition(int j) {
-        for (int i = GameField.FIELD_SIZE - 1; i > 0; i--) {
-            if (cells[i][j].getCellValue() != 0) {
-                int k = i - 1;
-
-                while (k >= 0) {
-                    if (cells[i][j].getCellValue() == cells[k][j].getCellValue()) {
-                        verticalCellsAddition(j, i, k);
-                        break;
-                    } else if (cells[k][j].getCellValue() != 0) {
+                    } else if (cells[tempIndex][col].getCellValue() != 0) {
                         break;
                     }
-                    k--;
+                    tempIndex++;
                 }
             }
         }
     }
 
-    private void downCellsShift(int j) {
-        for (int i = GameField.FIELD_SIZE - 2; i >= 0; i--) {
-            if (cells[i][j].getCellValue() != 0) {
-                while ((i != GameField.FIELD_SIZE - 1) && (cells[i + 1][j].getCellValue() == 0)) {
-                    cells[i + 1][j].setCellValue(cells[i][j].getCellValue());
-                    cells[i][j].setCellValue(0);
+    private void upCellsShift() {
+        for (row = 1; row < GameField.FIELD_SIZE; row++) {
+            if (cells[row][col].getCellValue() != 0) {
+                while ((row != 0) && (cells[row - 1][col].getCellValue() == 0)) {
+                    cells[row - 1][col].setCellValue(cells[row][col].getCellValue());
+                    cells[row][col].setCellValue(0);
 
                     hasMove = true;
-                    i++;
+                    row--;
                 }
             }
         }
     }
 
-    private void verticalCellsAddition(int j, int i, int k) {
-        cells[i][j].incrementPower();
-        GameField.incrementScore(cells[i][j].getCellValue());
-        cells[k][j].setCellValue(0);
+    private void downCellsAddition() {
+        for (row = GameField.FIELD_SIZE - 1; row > 0; row--) {
+            if (cells[row][col].getCellValue() != 0) {
+                tempIndex = row - 1;
+
+                while (tempIndex >= 0) {
+                    if (cells[row][col].getCellValue() == cells[tempIndex][col].getCellValue()) {
+                        verticalCellsAddition();
+                        break;
+                    } else if (cells[tempIndex][col].getCellValue() != 0) {
+                        break;
+                    }
+                    tempIndex--;
+                }
+            }
+        }
+    }
+
+    private void downCellsShift() {
+        for (row = GameField.FIELD_SIZE - 2; row >= 0; row--) {
+            if (cells[row][col].getCellValue() != 0) {
+                while ((row != GameField.FIELD_SIZE - 1) && (cells[row + 1][col].getCellValue() == 0)) {
+                    cells[row + 1][col].setCellValue(cells[row][col].getCellValue());
+                    cells[row][col].setCellValue(0);
+
+                    hasMove = true;
+                    row++;
+                }
+            }
+        }
+    }
+
+    private void verticalCellsAddition() {
+        cells[row][col].incrementPower();
+        GameField.incrementScore(cells[row][col].getCellValue());
+        cells[tempIndex][col].setCellValue(0);
 
         hasMove = true;
     }
 
-    private void leftCellsAddition(int i) {
-        for (int j = 0; j < GameField.FIELD_SIZE - 1; j++) {
-            if (cells[i][j].getCellValue() != 0) {
-                int k = j + 1;
+    private void leftCellsAddition() {
+        for (col = 0; col < GameField.FIELD_SIZE - 1; col++) {
+            if (cells[row][col].getCellValue() != 0) {
+                tempIndex = col + 1;
 
-                while (k < GameField.FIELD_SIZE) {
-                    if (cells[i][j].getCellValue() == cells[i][k].getCellValue()) {
-                        horizontalCellsAddition(i, j, k);
+                while (tempIndex < GameField.FIELD_SIZE) {
+                    if (cells[row][col].getCellValue() == cells[row][tempIndex].getCellValue()) {
+                        horizontalCellsAddition();
                         break;
-                    } else if (cells[i][k].getCellValue() != 0) {
-                        break;
-                    }
-                    k++;
-                }
-            }
-        }
-    }
-
-    private void leftCellsShift(int i) {
-        for (int j = 1; j < GameField.FIELD_SIZE; j++) {
-            if (cells[i][j].getCellValue() != 0) {
-                while ((j != 0) && (cells[i][j - 1].getCellValue() == 0)) {
-                    cells[i][j - 1].setCellValue(cells[i][j].getCellValue());
-                    cells[i][j].setCellValue(0);
-
-                    hasMove = true;
-                    j--;
-                }
-            }
-        }
-    }
-
-    private void rightCellsAddition(int i) {
-        for (int j = GameField.FIELD_SIZE - 1; j > 0; j--) {
-            if (cells[i][j].getCellValue() != 0) {
-                int k = j - 1;
-
-                while (k >= 0) {
-                    if (cells[i][j].getCellValue() == cells[i][k].getCellValue()) {
-                        horizontalCellsAddition(i, j, k);
-                        break;
-                    } else if (cells[i][k].getCellValue() != 0) {
+                    } else if (cells[row][tempIndex].getCellValue() != 0) {
                         break;
                     }
-                    k--;
+                    tempIndex++;
                 }
             }
         }
     }
 
-    private void rightCellsShift(int i) {
-        for (int j = GameField.FIELD_SIZE - 2; j >= 0; j--) {
-            if (cells[i][j].getCellValue() != 0) {
-                while ((j != GameField.FIELD_SIZE - 1) && (cells[i][j + 1].getCellValue() == 0)) {
-                    cells[i][j + 1].setCellValue(cells[i][j].getCellValue());
-                    cells[i][j].setCellValue(0);
+    private void leftCellsShift() {
+        for (col = 1; col < GameField.FIELD_SIZE; col++) {
+            if (cells[row][col].getCellValue() != 0) {
+                while ((col != 0) && (cells[row][col - 1].getCellValue() == 0)) {
+                    cells[row][col - 1].setCellValue(cells[row][col].getCellValue());
+                    cells[row][col].setCellValue(0);
 
                     hasMove = true;
-                    j++;
+                    col--;
                 }
             }
         }
     }
 
-    private void horizontalCellsAddition(int i, int j, int k) {
-        cells[i][j].incrementPower();
-        GameField.incrementScore(cells[i][j].getCellValue());
-        cells[i][k].setCellValue(0);
+    private void rightCellsAddition() {
+        for (col = GameField.FIELD_SIZE - 1; col > 0; col--) {
+            if (cells[row][col].getCellValue() != 0) {
+                tempIndex = col - 1;
+
+                while (tempIndex >= 0) {
+                    if (cells[row][col].getCellValue() == cells[row][tempIndex].getCellValue()) {
+                        horizontalCellsAddition();
+                        break;
+                    } else if (cells[row][tempIndex].getCellValue() != 0) {
+                        break;
+                    }
+                    tempIndex--;
+                }
+            }
+        }
+    }
+
+    private void rightCellsShift() {
+        for (col = GameField.FIELD_SIZE - 2; col >= 0; col--) {
+            if (cells[row][col].getCellValue() != 0) {
+                while ((col != GameField.FIELD_SIZE - 1) && (cells[row][col + 1].getCellValue() == 0)) {
+                    cells[row][col + 1].setCellValue(cells[row][col].getCellValue());
+                    cells[row][col].setCellValue(0);
+
+                    hasMove = true;
+                    col++;
+                }
+            }
+        }
+    }
+
+    private void horizontalCellsAddition() {
+        cells[row][col].incrementPower();
+        GameField.incrementScore(cells[row][col].getCellValue());
+        cells[row][tempIndex].setCellValue(0);
 
         hasMove = true;
     }
---
+++ src/test/java/ANSIGameFieldPrinterTest.java
@@ -0,0 +1,82 @@
+import game2048.*;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by employee on 10/19/15.
+ */
+public class ANSIGameFieldPrinterTest {
+
+    @Test
+    public void testCreateANSIGameFieldPrinter() {
+        new ANSIGameFieldPrinter(new PrintStream(System.out));
+    }
+
+    @Test
+    public void testPrintGameField() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+
+        GameFieldPrinter gameFieldPrinter = new ANSIGameFieldPrinter(printStream);
+
+        GameField gameField = new GameField(new RandomCellValueGenerator());
+
+        gameFieldPrinter.printGameField(gameField);
+
+        assertThat(baos.toString(), is(
+                "Score: 0\n" +
+                "-----------------------------\n" +
+                "|      |      |      |      |\n" +
+                "-----------------------------\n" +
+                "|      |      |      |      |\n" +
+                "-----------------------------\n" +
+                "|      |      |      |      |\n" +
+                "-----------------------------\n" +
+                "|      |      |      |      |\n" +
+                "-----------------------------\n"
+        ));
+
+        baos = new ByteArrayOutputStream();
+        printStream = new PrintStream(baos);
+
+        gameFieldPrinter = new ANSIGameFieldPrinter(printStream);
+
+        gameField = new GameField(new StaticCellValueGenerator(2));
+
+        for (int i = 0; i < GameField.FIELD_SIZE * GameField.FIELD_SIZE; i++) {
+            gameField.fillEmptyCell();
+        }
+
+        gameFieldPrinter.printGameField(gameField);
+
+        assertThat(baos.toString(), is(
+                "Score: 0\n" +
+                "-----------------------------\n" +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT + "|\n" +
+                "-----------------------------\n" +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT + "|\n" +
+                "-----------------------------\n" +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT + "|\n" +
+                "-----------------------------\n" +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT +
+                "|" + CellColor.DEFAULT + "     2" + CellColor.DEFAULT + "|\n" +
+                "-----------------------------\n"
+        ));
+    }
+}

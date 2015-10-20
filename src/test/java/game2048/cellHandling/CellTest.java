cd027a78506c312277f7369f893f366ab2c7a6be
Andriy Slipenko <andrews@interlink-ua.com>
2015-10-20 16:42:37 +0300

    day result, one test - one assert

--- src/test/java/game2048/cellHandling/CellTest.java
+++ src/test/java/game2048/cellHandling/CellTest.java
@@ -1,5 +1,6 @@
 package game2048.cellHandling;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,6 +13,11 @@ public class CellTest {
 
     Cell cell;
 
+    @Before
+    public void setUp() {
+        cell = new Cell();
+    }
+
     @Test
     public void testCreateCell() {
         new Cell();
@@ -19,10 +25,13 @@ public class CellTest {
 
     @Test
     public void testIsEmpty() {
-        cell = new Cell();
+        cell.setCellValue(0);
 
         assertThat(cell.isEmpty(), is(true));
+    }
 
+    @Test
+    public void testNotIsEmpty() {
         cell.setCellValue(2);
 
         assertThat(cell.isEmpty(), is(false));
@@ -30,19 +39,13 @@ public class CellTest {
 
     @Test
     public void testGetCellValue() {
-        cell = new Cell();
-
-        assertThat(cell.getCellValue(), is(0));
-
-        cell = new Cell(2);
+        cell.setCellValue(2);
 
         assertThat(cell.getCellValue(), is(2));
     }
 
     @Test
     public void testSetCellValue(){
-        cell = new Cell();
-
         cell.setCellValue(4);
 
         assertThat(cell.getCellValue(), is(4));
@@ -50,17 +53,17 @@ public class CellTest {
 
     @Test
     public void testIncrementPower() {
-        cell = new Cell();
-
-        cell.incrementPower();
-
-        assertThat(cell.getCellValue(), is(0));
-
         cell.setCellValue(2);
         cell.incrementPower();
 
         assertThat(cell.getCellValue(),is(4));
+    }
 
+    @Test
+    public void testRepeatedlyIncrementPower() {
+        cell.setCellValue(2);
+
+        cell.incrementPower();
         cell.incrementPower();
 
         assertThat(cell.getCellValue(),is(8));
